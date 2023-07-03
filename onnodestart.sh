146225cc9cd5ced156be4a652063702c3d339d76
tabanaoc <92966026+tabanaoc@users.noreply.github.com>
2023-07-03 14:58:15 +0900

    Update onnodestart.sh

--- onnodestart.sh
+++ onnodestart.sh
@@ -5,10 +5,11 @@ hostname >> /tmp/CustomActions.log
 id -u -n >> /tmp/CustomActions.log
 
 cat<<EOF >> /etc/sssd/sssd.conf
-#simple_allow_users = $1
+simple_allow_users = $1
 #simple_allow_users = tabata,naoto,test-tabata
-simple_allow_users = tabata,test-tabata
+#simple_allow_users = tabata,test-tabata
 EOF
 
 sudo systemctl restart sssd
 
+sudo usermod -aG docker $1
