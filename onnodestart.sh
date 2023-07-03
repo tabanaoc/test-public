0a5903470b1dc69260e3dace2f107bcae336bf1d
tabanaoc <92966026+tabanaoc@users.noreply.github.com>
2023-07-03 14:50:23 +0900

    Update onnodestart.sh

--- onnodestart.sh
+++ onnodestart.sh
@@ -5,8 +5,9 @@ hostname >> /tmp/CustomActions.log
 id -u -n >> /tmp/CustomActions.log
 
 cat<<EOF >> /etc/sssd/sssd.conf
-simple_allow_users = $1
+#simple_allow_users = $1
 #simple_allow_users = tabata,naoto,test-tabata
+simple_allow_users = tabata,test-tabata
 EOF
 
 sudo systemctl restart sssd
