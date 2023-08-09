b5fbc0db82aec5b05358ecc8a62b3f370745c797
tabanaoc <92966026+tabanaoc@users.noreply.github.com>
2023-08-10 01:27:50 +0900

    Update onnodestart.sh

--- onnodestart.sh
+++ onnodestart.sh
@@ -6,7 +6,7 @@ id -u -n >> /tmp/CustomActions.log
 
 cat<<EOF >> /etc/sssd/sssd.conf
 simple_allow_users = $1
-#simple_allow_users = tabata,naoto,test-tabata
+#simple_allow_users = tabata,test-tabata
 #simple_allow_users = tabata,test-tabata
 # 20230703 15:37
 EOF
